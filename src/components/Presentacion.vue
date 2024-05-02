83c308fe1045243b23179f875ce0814f07929fe1
alexcandela <candelalex22@gmail.com>
2024-05-02 13:55:23 +0200

    Update descargar portfolio

--- src/components/Presentacion.vue
+++ src/components/Presentacion.vue
@@ -6,7 +6,10 @@
     <div class="presentacion-info">
       <div class="presentacion-info-nombre"><h1>Hola, soy <span>Alex Candela</span></h1><img src="/src/assets/images/bot.png" alt="bot"></div>
       <div class="presentacion-info-desc"><p>Un apasionado por el mundo de la tecnología web. Motivado por la curiosidad y el deseo de seguir avanzando.</p></div>
-      <div class="presentacion-info-botones"><a href="https://www.linkedin.com/in/alex-candela-b31956228/" target="blank" class="btn btn-linkedin"><i class="fa-brands fa-linkedin"></i>Linkedin</a><a target="blank" href="https://github.com/alexcandela" class="btn btn-github"><i class="fa-brands fa-github"></i>Github</a><a href="/src/assets/files/CV_AlexCandela.pdf" download class="btn btn-resume"><i class="fa-solid fa-file"></i>Resume</a></div>
+      <div class="presentacion-info-botones">
+        <a href="https://www.linkedin.com/in/alex-candela-b31956228/" target="blank" class="btn btn-linkedin"><i class="fa-brands fa-linkedin"></i>Linkedin</a>
+        <a target="blank" href="https://github.com/alexcandela" class="btn btn-github"><i class="fa-brands fa-github"></i>Github</a>
+        <a href="/public/CV_AlexCandela.pdf" download class="btn btn-resume"><i class="fa-solid fa-file"></i>Resume</a></div>
     </div>
   </div>
 </template>
